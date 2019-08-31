773a465df6752907fad88e8bd0da64b7b3d50f8f
rk9109 <rk9109@gmail.com>
2019-08-31 00:57:03 -0700

    Update bloatware removal

--- scripts/install-general.ps1
+++ scripts/install-general.ps1
@@ -1,33 +1,71 @@
 # Description: Boxstarter Script
 # Author: Rinik Kumar
 
-
 # Prevent Windows MAXPATH Limit
 # https://github.com/chocolatey/boxstarter/issues/241
 $ChocoCachePath = "$env:USERPROFILE\AppData\Local\Temp\chocolatey"
 New-Item -Path $ChocoCachePath -ItemType Directory -Force
 
-
 # Temp Settings
 Disable-UAC
 Disable-MicrosoftUpdate
-
 
 # Remove Windows Features
 Disable-BingSearch
 Disable-GameBarTips
 
-
-# Remove Windows Bloatware
-$bloatware = @(
-    "Microsoft."
-    "Microsoft."
-    "Microsoft."
-    "Microsoft."
-)
-
 Function Remove-Bloatware {
-    # add parameter
+    $bloatware = @(
+        "Microsoft.3DBuilder"
+        "Microsoft.Appconnector"
+        "Microsoft.BingFinance"
+        "Microsoft.BingNews"
+        "Microsoft.BingSports"
+        "Microsoft.BingTranslator"
+        "Microsoft.BingWeather"
+        "Microsoft.BingFoodAndDrink"
+        "Microsoft.BingHealthAndFitness"
+        "Microsoft.BingTravel"
+        "Microsoft.FreshPaint"
+        "Microsoft.GamingServices"
+        "Microsoft.Microsoft3DViewer"
+        "Microsoft.MicrosoftPowerBIForWindows"
+        "Microsoft.MicrosoftSolitaireCollection"
+        "Microsoft.MicrosoftStickyNotes"
+        "Microsoft.MinecraftUWP"
+        "Microsoft.NetworkSpeedTest"
+        "Microsoft.OneConnect"
+        "Microsoft.People"
+        "Microsoft.Messaging"
+        "Microsoft.Print3D"
+        "Microsoft.Wallet"
+        "Microsoft.Windows.Photos"
+        "Microsoft.WindowsAlarms"
+        "Microsoft.WindowsCalculator"
+        "Microsoft.WindowsCamera"
+        "microsoft.windowscommunicationsapps"
+        "Microsoft.WindowsMaps"
+        "Microsoft.WindowsPhone"
+        "Microsoft.WindowsSoundRecorder"
+        "Microsoft.Xbox.TCUI"
+        "Microsoft.XboxApp"
+        "Microsoft.XboxGameOverlay"
+        "Microsoft.XboxGamingOverlay"
+        "Microsoft.XboxSpeechToTextOverlay"
+        "Microsoft.YourPhone"
+        "Microsoft.ZuneMusic"
+        "Microsoft.ZuneVideo"
+        "Microsoft.CommsPhone"
+        "Microsoft.ConnectivityStore"
+        "Microsoft.GetHelp"
+        "Microsoft.Getstarted"
+        "Microsoft.WindowsFeedbackHub"
+        "Microsoft.WindowsReadingList"
+        "Microsoft.MixedReality.Portal"
+        "Microsoft.ScreenSketch"
+        "Microsoft.YourPhone"
+        "Microsoft.Advertising.Xaml"
+    )
 
     foreach ($bloat in $bloatware) {
         Write-Output "Removing $bloat"
@@ -36,10 +74,10 @@ Function Remove-Bloatware {
     }
 }
 
+Remove-Bloatware
 
 # Enable WSL
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
-
 
 Function Install-Git {
     choco install -y git -params '"/GitAndUnixToolsOnPath"'
@@ -69,30 +107,25 @@ Function Install-Applications {
     cup --cacheLocation="$ChocoCachePath" bitwarden
 }
 
-
 # Install Packages
 Install-Git
 Install-Utilities
 Install-DevelopmentTools
 Install-Applications
 
-
 # Update Hostname
 $computerName = Read-Host 'Enter Hostname'
 Rename-Computer -NewName $computerName
 
-
 # Generate SSH Key
 $email = Read-Host "Enter Email"
 ssh-keygen -t rsa -b 4096 -C "$email"
-
 
 # Clean C:\
 del C:\eula*.txt
 del C:\install.*
 del C:\vcredist.*
 del C:\vc_red.*
-
 
 # Restore Temp Settings
 Enable-UAC
