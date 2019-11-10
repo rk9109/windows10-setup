1b22e3c7f10e0695d0051000141e96c7aef08dcb
Rinik Kumar <rk9109@gmail.com>
2019-11-10 04:51:52 -0500

    Update credentials

--- install.ps1
+++ install.ps1
@@ -1,5 +1,8 @@
 Import-Module Boxstarter.Chocolatey
 
+# Get Credentials
+$cred = Get-Credential domain\username
+
 # Create + Install Package
 New-PackageFromScript -Source scripts\install-general.ps1 -PackageName InstallGeneral
-Install-BoxstarterPackage -Package InstallGeneral
+Install-BoxstarterPackage -Package InstallGeneral -Credential $cred
